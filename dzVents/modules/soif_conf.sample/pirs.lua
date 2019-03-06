ad0f04d8ffc902d6422c55ad99bc37d1444fca13
moi <wxopwx@gmail.com>
2019-03-06 04:10:12 +0100

    fixes require + better documentation + pwn settings

--- dzVents/modules/soif_conf.sample/pirs.lua
+++ dzVents/modules/soif_conf.sample/pirs.lua
@@ -1,6 +1,6 @@
 local vars = {}
---local glob	=	require('soif_conf/globals')
-vars.pirs			={}
+local glob	=	require('soif_conf/globals')
+vars.pirs	={}
 local i=0
 
 --[[ #################################################################################################################
@@ -32,16 +32,20 @@ vars.def.growl.file			='pir'
 	PIRS definitions
 ################################################################################################################# --]]
 --[[ 
-PIRs,  indexed by Domoticz idx
-	- id		: (inf)				PIR idx
-	- name		: (str)				Name
-	- actions	: {str array}		Actions, Notification to do ? growl | kodi | kodis | indicator_light | indicator_flash | nab_tts | nab_file | nab_preset
-	- devices	: (int array)		Devices IDs to switch On (for action: 'switch')
-	- dur		: (int OR array) 	(seconds) Duration to stay On, can be an array (for each devices ). 0 = infinite time
-	- masters	: (int array)		Switch IDs used to determine if the PIR action is triggered or bypassed
-	- day_mode		: (int)				When to switch : (0) always | (1) during night | (2) during day
-	- nabaztag	: (str)				file, preset, or message for Nabaztag
-  - debounce	: (int)				Debounce time
+PIRs properties:
+All properties are optionals, except the 'id'
+- id		: (inf)				[REQUIRED] PIR idx from Domoticz
+- name		: (str)				Name of your PIR
+- actions	: {str OR table}	Actions, Notification to do ? growl | kodi | kodis | indicator_light | indicator_flash | nab_tts | nab_file | nab_preset
+- devices	: (int OR table)	Devices IDX to switch On (for action: 'switch')
+- dur		: (int OR table) 	(seconds) Duration to stay On, can be an array (for each devices ). 0 = infinite time
+- debounce	: (int)				Debounce time : 
+- day_mode	: (int)				When to switch : (0) always | (1)  night | (2) day
+- masters	: (int OR table)	Switch(es) IDX used to disable the PIR's actions
+- growl	: 	; (table)			Properties for growl actions
+	-file	: (str)				Icon url
+	-file	: (str)				Icon url
+- nabaztag	: (str)				file, preset, or message for Nabaztag
 
 --]]
 
@@ -66,10 +70,10 @@ vars.pirs[i].actions	={'switch','growl'}
 vars.pirs[i].devices	={88, 328}	--Simu1 Cuis, Sej.S
 vars.pirs[i].masters	={450} -- 
 vars.pirs[i].dur		=2*60
-vars.pirs[i].day_mode		=1	-- 0 always, 1 night, 2 day
+vars.pirs[i].day_mode	=1	-- 0 always, 1 night, 2 day
 vars.pirs[i].debounce	=45
 vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="pir_walk"
+vars.pirs[i].growl.file="pir_hall"
 
 --------------------------------------
 i=i+1
@@ -80,9 +84,9 @@ vars.pirs[i].actions	={'switch','growl'}
 vars.pirs[i].devices	={413,415}	--, socket 4, leds
 vars.pirs[i].masters	={451} -- 
 vars.pirs[i].dur		={60,180}
-vars.pirs[i].day_mode		=1
+vars.pirs[i].day_mode	=1
 vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="pir_walk"
+vars.pirs[i].growl.file="pir_bureau"
 
 --------------------------------------
 i=i+1
@@ -95,34 +99,12 @@ vars.pirs[i].masters	={452} --
 vars.pirs[i].dur		=30
 vars.pirs[i].day_mode		=0	-- 0 always, 1 night, 2 day
 vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="pir_walk"
+vars.pirs[i].growl.file="pir_buanderie"
 
 
 --------------------------------------
 --- OUTSIDE --------------------------
 --------------------------------------
-i=i+1
-vars.pirs[i]			={}
-vars.pirs[i].id			=423
-vars.pirs[i].name		='Reverbere'
-vars.pirs[i].actions	={'switch', 'kodis', 'growl', 'indic_light'}
-vars.pirs[i].devices	={272} -- reverb
-vars.pirs[i].masters	={453} -- 
-vars.pirs[i].dur		=5*60
-vars.pirs[i].day_mode		=1	-- 0 always, 1 night, 2 day
-vars.pirs[i].debounce	=20
-vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="alert"
-
---------------------------------------
-i=i+1
-vars.pirs[i]			={}
-vars.pirs[i].id			=425
-vars.pirs[i].name		='Poulailler'
-vars.pirs[i].actions	={'switch', 'kodis', 'growl', 'indic_light'}
-vars.pirs[i].devices	={388} -- Salon2, 271 Table Sej
-vars.pirs[i].masters	={454} -- 
-vars.pirs[i].day_mode		=1
 
 --------------------------------------
 i=i+1
@@ -136,7 +118,33 @@ vars.pirs[i].day_mode		=1
 vars.pirs[i].nabaztag	='pir'
 vars.pirs[i].debounce	=30
 vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="alert1"
+vars.pirs[i].growl.file="pir_entree"
+
+--------------------------------------
+i=i+1
+vars.pirs[i]			={}
+vars.pirs[i].id			=423
+vars.pirs[i].name		='Reverbere'
+vars.pirs[i].actions	={'switch', 'kodis', 'growl', 'indic_light'}
+vars.pirs[i].devices	={272} -- reverb
+vars.pirs[i].masters	={453} -- 
+vars.pirs[i].dur		=5*60
+vars.pirs[i].day_mode		=1	-- 0 always, 1 night, 2 day
+vars.pirs[i].debounce	=20
+vars.pirs[i].growl	={}
+vars.pirs[i].growl.file="pir_reverbere"
+
+--------------------------------------
+i=i+1
+vars.pirs[i]			={}
+vars.pirs[i].id			=425
+vars.pirs[i].name		='Poulailler'
+vars.pirs[i].actions	={'switch', 'kodis', 'growl', 'indic_light'}
+vars.pirs[i].devices	={388} -- Salon2, 271 Table Sej
+vars.pirs[i].masters	={454} -- 
+vars.pirs[i].day_mode		=1
+vars.pirs[i].growl	={}
+vars.pirs[i].growl.file="pir_poulailler"
 
 --------------------------------------
 i=i+1
@@ -151,7 +159,7 @@ vars.pirs[i].day_mode		=1
 vars.pirs[i].nabaztag	='pir'
 vars.pirs[i].debounce	=20
 vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="pir_red"
+vars.pirs[i].growl.file="pir_portail"
 
 --------------------------------------
 i=i+1
@@ -160,8 +168,10 @@ vars.pirs[i].id			=433
 vars.pirs[i].name		='Terrasse'
 vars.pirs[i].devices	={388,271} -- Salon2, Table Sej
 vars.pirs[i].masters	={457} -- 
-vars.pirs[i].day_mode		=1
+vars.pirs[i].day_mode	=1
 vars.pirs[i].actions	={'switch', 'kodis','growl','indic_light'}
+vars.pirs[i].growl		={}
+vars.pirs[i].growl.file	="pir_terrasse"
 
 --------------------------------------
 i=i+1
@@ -171,8 +181,8 @@ vars.pirs[i].name		='Boite Au Lettre'
 vars.pirs[i].message	='Le Facteur vient de passer'
 vars.pirs[i].nabaztag	='facteur'
 vars.pirs[i].actions	={'kodis','growl','indic_flash','nab_file'}
-vars.pirs[i].growl	={}
-vars.pirs[i].growl.file="bal"
+vars.pirs[i].growl		={}
+vars.pirs[i].growl.file="pir_bal"
 
 
 
@@ -188,11 +198,11 @@ vars.pirs[i].growl.file="bal"
 
 i=i+1
 vars.pirs[i]				={}
---vars.pirs[i].id				=glob.but_test
-vars.pirs[i].name			='Test Button Xiamoi'
---vars.pirs[i].message		="Je viens d'appuyer sur le bouton"
+vars.pirs[i].id				=glob.but_test
+vars.pirs[i].name			='Test  Xiaomi Button'
+vars.pirs[i].message		="Je viens d'appuyer sur le bouton"
 vars.pirs[i].actions		={'switch','growl'}	-- 'switch', 'nab_file', 'kodi', 'kodis','indic_flash'
-vars.pirs[i].devices		={415} -- Salon2, 271 Table Sej
+vars.pirs[i].devices		={271} -- Salon2, 271=Table Sej, 415=bureau
 --vars.pirs[i].masters		={387} -- 387=Salon1 
 --vars.pirs[i].dur			={5}
 vars.pirs[i].dur			=1
@@ -200,7 +210,7 @@ vars.pirs[i].day_mode		=0
 vars.pirs[i].nabaztag		='pir'
 vars.pirs[i].debounce		=2
 vars.pirs[i].growl			={}
-vars.pirs[i].growl.file		="bal"
+vars.pirs[i].growl.file		="test"
 vars.pirs[i].growl.priority="high"
 
 
