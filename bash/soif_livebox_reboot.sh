278f9c49a942b5bf20acb4d2c421734197481051
moi <wxopwx@gmail.com>
2019-03-09 16:59:03 +0100

    adds livebox renew_ip and reboot scipts

---
+++ bash/soif_livebox_reboot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source `dirname $0`/soif_livebox.inc
+
+connectLivebox 	# connect and grab myFileCookies and myContextID -------
+
+# reboot ---------------------------------------------------------------
+# curl -s -o /dev/null -b "$myFileCookies" -X POST -H 'Content-Type: application/x-sah-ws-4-call+json' -H "X-Context: $myContextID" -d "{\"service\":\"NMC\",\"method\":\"reboot\",\"parameters\":{}}" http://$myLivebox/ws
+STATUS=$(curl -s  -b "$myFileCookies" -X POST -H 'Content-Type: application/x-sah-ws-4-call+json' -H "X-Context: $myContextID" -d "{\"service\":\"NMC\",\"method\":\"reboot\",\"parameters\":{}}" http://$myLivebox/ws 2>&1 )
+
+# todo check return status. Should be : {"status":true} ----------------
+#echo "$STATUS"
+
+# quit -----------------------------------------------------------------
+#closeConnection ## disconnect
+exit 0
---
+++ bash/soif_livebox_renew_ip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source `dirname $0`/soif_livebox.inc
+
+connectLivebox 	# connect and grab myFileCookies and myContextID -------
+
+# renew dhcp -----------------------------------------------------------
+OUT_NULL=''
+#OUT_NULL=-"-o /dev/null"
+
+STATUS_OFF=$(	curl -s "$OUT_NULL" -b "$myFileCookies" -X POST -H 'Content-Type: application/x-sah-ws-4-call+json' -H "X-Context: $myContextID" -d "{\"service\":\"NeMo.Intf.data\",\"method\":\"setFirstParameter\",\"parameters\":{\"name\":\"Enable\",\"value\":0,\"flag\":\"dhcp\",\"traverse\":\"down\"}}" http://$myLivebox/ws )
+sleep 1
+STATUS_ON=$(	curl -s "$OUT_NULL" -b "$myFileCookies" -X POST -H 'Content-Type: application/x-sah-ws-4-call+json' -H "X-Context: $myContextID" -d "{\"service\":\"NeMo.Intf.data\",\"method\":\"setFirstParameter\",\"parameters\":{\"name\":\"Enable\",\"value\":1,\"flag\":\"dhcp\",\"traverse\":\"down\"}}" http://$myLivebox/ws )
+
+
+# quit -----------------------------------------------------------------
+#closeConnection ## disconnect
+exit 0
