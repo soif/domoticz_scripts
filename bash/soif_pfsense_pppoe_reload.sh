73a89a56e429b1c92a2836204375d3cd657e59ee
moi <wxopwx@gmail.com>
2019-03-09 14:50:39 +0100

    pfsense use conf file variables

--- bash/soif_pfsense_pppoe_reload.sh
+++ bash/soif_pfsense_pppoe_reload.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+source `dirname $0`/soif_conf.conf
 
-# https://forum.netgate.com/topic/86883/help-on-disconnect-and-reconnect-a-pppoe-connection-by-command-line/5
-
-# ssh net@gate.lo.lo " /usr/local/sbin/pfSctl -c 'interface reload opt1'; /usr/bin/logger -t ppp 'PPPoE reload executed on OPT1'; exit 0 " 
+# ----------------------------------------------------------------------------------------
+HOST=${SOIF_GLOB[pfsense_host]}
+USER=${SOIF_GLOB[pfsense_user]}
+IF=${SOIF_GLOB[pfsense_interface]}
 
 #IF=opt1
 IF=wan
 
- ssh net@gate.lo.lo " /usr/local/sbin/pfSctl -c 'interface reload $IF'; /usr/bin/logger -t ppp 'PPPoE reload executed on $IF'; exit 0 " 
+# ----------------------------------------------------------------------------------------
+# https://forum.netgate.com/topic/86883/help-on-disconnect-and-reconnect-a-pppoe-connection-by-command-line/5
+# ssh net@gate.lo.lo " /usr/local/sbin/pfSctl -c 'interface reload wan'; /usr/bin/logger -t ppp 'PPPoE reload executed on WAN'; exit 0 " 
 
+ACT="interface reload $IF"
+LOG="External Dzvent Script soif_pfsense is restarting PPPoE on the $IF Interface......."
+ssh ${USER}@${HOST} "/usr/local/sbin/pfSctl -c '$ACT';	/usr/bin/logger -t soif '$LOG'; exit 0; " 
 exit 0
