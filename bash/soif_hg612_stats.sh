6bdde0b4a14d2c66e094a316500a99666a015f5b
moi <wxopwx@gmail.com>
2019-03-09 13:17:15 +0100

    initial hg612_stats commit

---
+++ bash/soif_hg612_stats.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+source `dirname $0`/soif_conf.conf
+
+URL="http://${SOIF_GLOB[h612_host]}/html/status/xdslStatus.asp"
+
+RAW=$(wget -qO- "$URL")
+CLEAN=`echo "$RAW" | head -n 1 | sed -E s/.*stDsl//g | sed -E s/\"//g  | sed -E "s/,/ /g" `
+
+#echo "$RAW"
+#echo "----"
+#echo "$CLEAN"
+#echo "----"
+
+declare -a KEYS
+declare -a VALUES
+
+KEYS+=('state');	VALUES+=(` echo "$CLEAN" | awk '{print $2}' `)
+KEYS+=('type');		VALUES+=(` echo "$CLEAN" | awk '{print $3}' `)
+
+KEYS+=('up');		VALUES+=(` echo "$CLEAN" | awk '{print $4}' `000)
+KEYS+=('down');		VALUES+=(` echo "$CLEAN" | awk '{print $5}' `000)
+
+KEYS+=('max_up');		VALUES+=(` echo "$CLEAN" | awk '{print $8}' `000)
+KEYS+=('max_down');		VALUES+=(` echo "$CLEAN" | awk '{print $9}' `000)
+
+KEYS+=('att_up');		VALUES+=(` echo "$CLEAN" | awk '{print $10/10}' `)
+KEYS+=('att_down');		VALUES+=(` echo "$CLEAN" | awk '{print $11/10}' `)
+
+KEYS+=('pwr_up');		VALUES+=(` echo "$CLEAN" | awk '{print $12/10}' `)
+KEYS+=('pwr_down');		VALUES+=(` echo "$CLEAN" | awk '{print $13/10}' `)
+
+#KEYS+=('snr_up');		VALUES+=(` echo "$CLEAN" | awk '{print $14/10}' `)
+#KEYS+=('snr_down');		VALUES+=(` echo "$CLEAN" | awk '{print $15/10}' `)
+
+
+JSON="{"
+for K in "${!VALUES[@]}"; do 
+#	echo ${KEYS[$K]}:	${VALUES[$K]}; 
+	JSON+='"'
+	JSON+=${KEYS[$K]}
+	JSON+='": "'
+	JSON+=${VALUES[$K]}
+	JSON+='", '
+done
+JSON+="'end':''}"
+
+echo "$JSON"
+exit 0
